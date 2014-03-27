b223abbe4e08fc8ae42d174c806f04aff1b4b718
aktoca <axtoca14@gmail.com>
2014-03-27 12:47:41 -0400

    set upper-right coordinates

---
+++ mars_rover.rb
@@ -0,0 +1,18 @@
+def initialize
+  puts "Enter upper-right x coordinate"
+  x = gets.chomp.to_i
+  puts "Enter upper-right y coordinate"
+  y = gets.chomp.to_i
+  @coordinates = [x, y]
+
+  puts coordinates.inspect
+end
+
+def position()
+
+end
+
+def explore()
+
+end
+

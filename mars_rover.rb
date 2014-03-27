64bde6fd4bac0283e22cbd9b396e0cd41577709f
aktoca <axtoca14@gmail.com>
2014-03-27 13:18:25 -0400

    Maybe some progress

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,20 +1,33 @@
-class Rover
+class Plateau
   def initialize(xr,yr)
-    @lr_coords = [0,0]
+    @lowr_x = 0
+    @lowr_y = 0
     # ask for coords
     # puts "Enter upper-right x coordinate"
     # x = gets.chomp.to_i
     # puts "Enter upper-right y coordinate"
     # y = gets.chomp.to_i
-    @ur_coords = [xr, yr]
+    @upr_x = xr
+    @upr_y = yr 
 
-    puts @coordinates.inspect
+  end
+end
+
+class Rover < Plateau
+  def initialize (x,y)
+    super
+    puts @lowr_x
+    puts @lowr_y
+    puts x
+    puts y
   end
 
   def position(l_r,u_d,facing)
-    l_r
-    u_d
-    facing
+    puts l_r
+    puts @lowr_x
+    puts u_d
+    puts @lowr_y
+    #Rover.new(@lowr_x + l_r, @lowry + u_d)
   end
 
   def explore()
@@ -23,4 +36,5 @@ class Rover
 end
 
 rex = Rover.new(5,5)
-rex.position(1,2,'N')
+move.position(1,2,'N')
+puts move

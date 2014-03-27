d69114c4019dc5569d209baecce7ccd394815627
aktoca <axtoca14@gmail.com>
2014-03-27 12:57:33 -0400

    added basic positino method not=working

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,18 +1,26 @@
-def initialize
-  puts "Enter upper-right x coordinate"
-  x = gets.chomp.to_i
-  puts "Enter upper-right y coordinate"
-  y = gets.chomp.to_i
-  @coordinates = [x, y]
+class Rover
+  def initialize(xr,yr)
+    @lr_coords = [0,0]
+    # ask for coords
+    # puts "Enter upper-right x coordinate"
+    # x = gets.chomp.to_i
+    # puts "Enter upper-right y coordinate"
+    # y = gets.chomp.to_i
+    @ur_coords = [xr, yr]
 
-  puts coordinates.inspect
+    puts @coordinates.inspect
+  end
+
+  def position(l_r,u_d,facing)
+    l_r
+    u_d
+    facing
+  end
+
+  def explore()
+
+  end
 end
 
-def position()
-
-end
-
-def explore()
-
-end
-
+rex = Rover.new(5,5)
+rex.position(1,2,'N')
